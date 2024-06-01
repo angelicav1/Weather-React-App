741ab5a6944fa8edb4704c56e603ec6051ae6a4a
angelicav1 <114718142+angelicav1@users.noreply.github.com>
2024-06-01 13:57:31 -0700

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App"> Weather App
-  </div>
+  return (
+    <div className='App'>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+         <a href="https://wildbyte.org/" target="_blank">Angelica Victoria</a> and is{' '}
+        <a
+          href='https://github.com/angelicav1/weather-react-app'
+          target='_blank'
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
     
