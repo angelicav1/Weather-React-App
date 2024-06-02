83b2bdb7161655b62a80f77a70c66bcc151a96d3
angelicav1 <114718142+angelicav1@users.noreply.github.com>
2024-06-01 20:11:00 -0700

    resolved deployment error
    added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ export default function App() {
     <div className='App'>
       <h1>Weather App</h1>
       <footer>
-        This project was coded by{" "}
-         <a href="https://wildbyte.org/" target="_blank">Angelica Victoria</a> and is{' '}
+        This project was coded by{' '}
+        <a href='https://wildbyte.org/' target='_blank' rel='noreferrer'>
+          Angelica Victoria
+        </a>{' '}
+        and is{' '}
         <a
           href='https://github.com/angelicav1/weather-react-app'
           target='_blank'
+          rel='noreferrer'
         >
           open-sourced on GitHub
         </a>
