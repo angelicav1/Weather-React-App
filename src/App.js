e4f9534d0d499a78c0c7491fa5615683df39408d
angelicav1 <114718142+angelicav1@users.noreply.github.com>
2024-06-01 20:48:55 -0700

    added bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import './App.css';
 export default function App() {
   return (
     <div className='App'>
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{' '}
-        <a href='https://wildbyte.org/' target='_blank' rel='noreferrer'>
-          Angelica Victoria
-        </a>{' '}
-        and is{' '}
-        <a
-          href='https://github.com/angelicav1/weather-react-app'
-          target='_blank'
-          rel='noreferrer'
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className='container'>
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{' '}
+          <a href='https://wildbyte.org/' target='_blank' rel='noreferrer'>
+            Angelica Victoria
+          </a>{' '}
+          and is{' '}
+          <a
+            href='https://github.com/angelicav1/weather-react-app'
+            target='_blank'
+            rel='noreferrer'
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
